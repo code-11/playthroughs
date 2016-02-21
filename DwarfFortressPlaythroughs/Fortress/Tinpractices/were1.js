518fe564229c75b4b9669388cc8320956830b848
code-11 <brendan.ritter@yahoo.com>
2016-02-21 18:04:18 -0500

    Got through another folder of commentary. Need to restructure menu next time.

---
+++ DwarfFortressPlaythroughs/Fortress/Tinpractices/were1.js
@@ -0,0 +1,79 @@
+var the_walkthrough=new walkthrough();
+
+the_walkthrough.create_block("Were1/1_emerge.png","The Beginning",
+	[
+		"While the dwarves were peacefully milling about doing silly inconsequential things like making food, a sinister force approached. ",
+		"The Were...weasel. How interesting. Its tiny but voracious demeanor frightened the dwarves, who at this point had no military to speak of."
+	]
+);
+
+the_walkthrough.create_block("Were1/2_engage.png","Attack! Attack!",
+	[
+		"Undaunted, the dwarves closest to the menace took up arms and decided to confront the beast, consequences be damned."
+	]
+);
+
+the_walkthrough.create_block("Were1/3_fight.png","Fight Club",
+	[
+		"Initial results were good. The wereweasel was not expecting such an assault. However, several of the dwarves were so eager to engage that they forgot their weapons, claiming they were 'wrestlers'"
+	]
+);
+the_walkthrough.create_block("Were1/4_fight.png","A Reversal of Fate",
+	[
+		"As one might expect, these 'wrestlers' fared poorly against the night menace, quickly succumbing to its superior wrestling moves, bites being the worst of the lot. ",
+		"As the wrestlers were bitten, a strange and uncomfortable feeling came over them. They all knew the stories. They were about to divine fantasy from real horror first hand."
+	]
+);
+the_walkthrough.create_block("Were1/5_bridge.png","Bridge",
+	[
+		"Thankfully one of the dwarves within the fortress itself retained his composure and smartly decided to raise the drawbridge, unfortunately trapping the militia outside with the beast.",
+		"When asked about his actions he simply noted that there was no way it could get them now!"
+	]
+);
+the_walkthrough.create_block("Were1/6_aftermath.png","Aftermath",
+	[
+		"The fight soon turned bloody and the poor unarmed dwarves suffered the worst of it. The beast tore into their flesh, flinging them off the side of the nearby cliff where they bled out. A sad day for the fortress."
+	]
+);
+the_walkthrough.create_block("Were1/7_toll.png","The Toll",
+	[
+		"The dwarves were completely unprepared. That much was clear. But when the horrible carnage finally ended, several dwarves lay dead."	
+	]
+);
+the_walkthrough.create_block("Were1/8_normal.png","Normality",
+	[
+		"The survivors reluctantly emerged from their stockade and hauled the bodies of their comrades to an area some ways away from the fort."	
+	]
+);
+the_walkthrough.create_block("Were1/9_artifact.png","Artifact",
+	[
+		"Despite the somber moment, one of the more creative dwarves immorialized the occasion by creating an artifact of astounding quality."	
+	]
+);
+the_walkthrough.create_block("Were1/10_emerge.png","Wait. Where did the Wereweasel go?",
+	[
+		"Nish surveyed the carnage. So where did you put the body of wereweasel? The dwarves stared at each other a moment before realization hit them."	
+	]
+);
+the_walkthrough.create_block("Were1/13_attack.png","A Miner Setback",
+	[
+		"Civilians were caught up in the ambush. Fortunately, a miner and his pick put an end to the beast for good."	
+	]
+);
+the_walkthrough.create_block("Were1/11_dead.png","For real this time",
+	[
+		"This time, Nish checked the body himself.",
+		" Yup. Thats dead alright."	
+	]
+);
+the_walkthrough.create_block("Were1/12_toll.png","The Toll",
+	[
+		"Two additional dwarves were dead. But worse, some of the survivors didn't look very good..."	
+	]
+);
+the_walkthrough.create_block("Were1/14_containment.png","Quarantine",
+	[
+		"When the dwarves awoke, they found a new structure in front of their fort. Heeding the old stories, the masons had taken it upon themselves to to seal the unfortunate dwarves behind stone. Its the only way to make sure, they said somberly."
+	]
+);
+the_walkthrough.create_high_nav("top_menu");

0c4432540fcd23fbcfbf57945c209dd2bac6f4ef
code-11 <brendan.ritter@yahoo.com>
2016-02-23 13:11:36 -0500

    Got through another folder.

---
+++ DwarfFortressPlaythroughs/Fortress/Tinpractices/were3.js
@@ -0,0 +1,78 @@
+var the_walkthrough=new walkthrough();
+
+the_walkthrough.create_block("Were3/1_overview.png","Ghost Busters",
+	[
+		"The problem with wereweasels is that they create a lot of ghostly dwarves. The stone masons furiously built slab after slab.",
+	]
+);
+
+the_walkthrough.create_block("Were3/2_prison.png","Personal Prison",
+	[
+		"One of the previous bitten dwarves was too far from any prison. The masons decided to seal in his room while he slept."
+	]
+);
+
+the_walkthrough.create_block("Were3/3_overview.png","Overview",
+	[
+		"The fortress was now firmly established. Despite not selling much to the caravan last year, the dwarves were shocked when Nish told them they had exported nothing.",
+		"That can't be right!"
+	]
+);
+the_walkthrough.create_block("Were3/4_archery.png","Archery",
+	[
+		"Based on previous results of getting bitten, the military dwarves requested archery targets to train on.",
+		"The dwarves also became annoyed when the outpost liason refused to leave. Freeloader."
+	]
+);
+the_walkthrough.create_block("Were3/5_brewer.png","A Skilled Mead",
+	[
+		"Nish noticed that the stocks of alcohol were running low. A short exploration revealed that only one dwarf was making any. However, he was very good at it."
+	]
+);
+the_walkthrough.create_block("Were3/6_emerge.png","Another Wereweasel",
+	[
+		"Another weasel showed its face."
+	]
+);
+the_walkthrough.create_block("Were3/7_engage.png","Engage",
+	[
+		"The military took its time in getting ready. Some of the citizens decided to run straight towards the wereweasel. They had no job, just decided to make things interesting."	
+	]
+);
+the_walkthrough.create_block("Were3/8_engage.png","Sowing Discontent",
+	[
+		"Fortunately, the wereweasel had its focus set on some of the fortresses livestock."	
+	]
+);
+the_walkthrough.create_block("Were3/9_report.png","The report",
+	[
+		"The military, more concerned with rushing into things than strategy, soon brutally murdered the wereweasel.",
+		"After the event Nish took stock of who had been bitten. A hunting dog, two recruits... and me! "	
+	]
+);
+the_walkthrough.create_block("Were3/10_quarantine.png","Quarantine",
+	[
+		"No there must be some mistake! I'm innocent!",
+		"Nish's cries were ignored as he was sealed within the walls with the other recruit."	
+	]
+);
+the_walkthrough.create_block("Were3/11_quarantine.png","Legendary",
+	[
+		"After several months behind stone walls, it turned out one of the recruits was actually innocent, and his cries distrubingly ceased.",
+		"Meanwhile, another dwarf tried to commemorate the violence yet again. However, finding the material at his disposal not sufficient he began to foam at the mouth.",
+	]
+);
+
+the_walkthrough.create_block("Were3/12_quarantine.png","Berserk Abridged",
+	[
+		"The stone masons, now skilled at this sort of thing, sealed him into his workshop.",
+	]
+);
+the_walkthrough.create_block("Were3/13_quarantine.png","Nish's End",
+	[
+		"It turns out Nish was innocent. The recruit on the other hand was not. The cries disturbed everyone, and all were glad when they were replaced with the usual beastial growling now common to many walls in the fortress.",
+		"With better hindsight, perhaps the convesion is not automatic. The masons considered putting any future quarantines in their own prison just to make sure."
+	]
+);
+
+the_walkthrough.create_high_nav("top_menu");

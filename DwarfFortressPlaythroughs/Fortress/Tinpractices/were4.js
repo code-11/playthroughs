23a7695f625aa394af91f68ec12d6f1594468ff1
code-11 <brendan.ritter@yahoo.com>
2016-02-29 16:00:01 -0500

    Got through another folder.

---
+++ DwarfFortressPlaythroughs/Fortress/Tinpractices/were4.js
@@ -0,0 +1,99 @@
+var the_walkthrough=new walkthrough();
+
+the_walkthrough.create_block("Were4/1_teleport.png","Teleport",
+	[
+		"The problem first came to the nobles when one of them complained he had been brutally murdered.",
+		"Closer investigation revealed that the wereweasel had seemingly teleported out of his cell"
+	]
+);
+the_walkthrough.create_block("Were4/2_report.png","Rigoth's end",
+	[
+		"The wereweasel was dealt with harshly, but not before it was able to bite one of the masons."
+	]
+);
+the_walkthrough.create_block("Were4/3_deathmachine.png","Death Machine",
+	[
+		"Worried that their quarantine strategy had somehow failed, the nobles ordered the construction of a machine capable of drowning dwarves.",
+		"The masons shrugged and started preparation."
+	]
+);
+the_walkthrough.create_block("Were4/4_emerge.png","Familiar face?",
+	[
+		"No sooner had construction started, when another feral wereweasel was spotted.",
+		"Something looked familiar about this wereweasel..."
+	]
+);
+the_walkthrough.create_block("Were4/5_teleport.png","Teleport",
+	[
+		"As soon as he was spotted, he seemingly teleported right on top of a child dwarf!"
+	]
+);
+the_walkthrough.create_block("Were4/6_report.png","Fight",
+	[
+		"A fierce fight broke out."
+	]
+);
+the_walkthrough.create_block("Were4/7_synthesis_report.png","Fight club",
+	[
+		"More dwarves rushed to the fight. And more were bitten..."
+	]
+);
+the_walkthrough.create_block("Were4/8_glitch.png","Glitch?",
+	[
+		"The nobles determined the cause of these seeming teleportations. Several holes had somehow been dug into the fortress!"
+	]
+);
+the_walkthrough.create_block("Were4/9_fight.png","The more the merrier",
+	[
+		"The dwarves, starved of new clothing by an administrative oversight launched themselves into the battle hoping to claim the rags of their deceased neighbors.",
+		"The inexperienced yet plentiful dwarf civilians soon wrestled the werebeast to bits."
+	]
+);
+the_walkthrough.create_block("Were4/10_machine.png","The machine",
+	[
+		"The two bitten who had survived were escorted to a specially designated hospital zone... in the death machine."
+	]
+);
+the_walkthrough.create_block("Were4/11_sucks.png","Sucks",
+	[
+		"One of the dwarves affected, a legendary mason, found the situation eerily familiar."
+	]
+);
+the_walkthrough.create_block("Were4/12_the_act.png","The act",
+	[
+		"Not wanting to dirty their own hands, the nobles ordered the level to be pulled."
+	]
+);
+the_walkthrough.create_block("Were4/13_the_act.png","Go with the flow!",
+	[
+		"The waters rushed in..."
+	]
+);
+the_walkthrough.create_block("Were4/14_partial_success.png","Nothing left!",
+	[
+		"...murdering one dwarf successfully (blinking)"
+	]
+);
+the_walkthrough.create_block("Were4/15_partial_success.png","Partial success?",
+	[
+		"The other could swim however."
+	]
+);
+
+the_walkthrough.create_block("Were4/16_failure.png","Failure",
+	[
+		"This proved to be a problem. The design of the machine was called into question."
+	]
+);
+the_walkthrough.create_block("Were4/17_funny.png","Legacy",
+	[
+		"The survivor was sealed in like normal since the nobles didn't want to risk waiting till the device rearmed."
+	]
+);
+the_walkthrough.create_block("Were4/18_bug.png","Bug!",
+	[
+		"Consulting ancient books of knowledge, the nobles determined that cutting down the trees had angered some minor elf spirits.",
+		"Luckily, the problem could be rectified by just building floors after cutting down trees."
+	]
+);
+the_walkthrough.create_high_nav("top_menu");

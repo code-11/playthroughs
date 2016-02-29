aea3da30f7b565721ff337766af583eede8f0e9d
code-11 <brendan.ritter@yahoo.com>
2016-02-28 23:49:23 -0500

    Got through another folder.

---
+++ DwarfFortressPlaythroughs/Fortress/Tinpractices/attack1.js
@@ -0,0 +1,75 @@
+var the_walkthrough=new walkthrough();
+
+the_walkthrough.create_block("Attack1/evil_arrive1.png","Attack!",
+	[
+		"A vile force of evil. The hunters saw it coming a mile off, but the smell that proceeded them was noticeable even before that."
+	]
+);
+the_walkthrough.create_block("Attack1/battle_plan1.png","The field of battle!",
+	[
+		"The recruits nervously assembled on the plain below the fortress and watched the goblins and their troll auxiliaries cross the river."
+	]
+);
+the_walkthrough.create_block("Attack1/battle_plan2.png","The actual battlefield",
+	[
+		"The militia captain suddenly recalled everyone to the outer walls. Nearly two full squads of mixed weapon wielding dwarves reassembled as the gate to the fortress was closed. Unfortunately, this trapped some civilians outside. They decided, intelligently, to stay behind the military dwarves."
+	]
+);
+the_walkthrough.create_block("Attack1/battle_plan3.png","Engagement",
+	[
+		"The two groups met at the north of the fortress. This forced the goblins to fight uphill and in a bottleneck, reducing the advantage of their numbers.",
+		"Some dwarves were terrified and abandoned their posts. Others sought out glory and detached to run after goblins coming from the sides. The majority listened to the battle plan though.",
+		"The civilians scattered in terror."
+	]
+);
+the_walkthrough.create_block("Attack1/report1.png","Initial report",
+	[
+		"From what could be pieced together after, the initial charge went somewhat poorly. One of the squad leaders fell to a goblin almost instantly.",
+		"In an odd moment of self reflection from the foe, even the goblin seemed taken aback by his own act."
+	]
+);
+the_walkthrough.create_block("Attack1/armor_fail.png","Attack!",
+	[
+		"One of the major complaints from the survivors was their lack of armor and quality weapons.",
+		"They went so far as to heretically claim that the goblins armor was as good as the weapons the dwarves were issued!"
+	]
+);
+the_walkthrough.create_block("Attack1/puppies.png","Think of the puppies!",
+	[
+		"The evil fiends even killed one of the fortress pets, a beloved stray puppy. This was too much for one dwarf. Something had to be done!"
+	]
+);
+the_walkthrough.create_block("Attack1/hammer_lord.png","Lord of war",
+	[
+		"Unlocking power unknown even to themselves, the hammer dwarf Edzul launched himself into the fray...",
+		"...and completely slaughtered the goblins!"
+	]
+);
+the_walkthrough.create_block("Attack1/hammer_lord2.png","Hammer of the fortress",
+	[
+		"The dwarves rallied behind this figure, but they almost didn't need to.",
+		"The hammer lord waded through the battle like a mighty storm, dispatching goblins and trolls alike with frightening power."
+	]
+);
+the_walkthrough.create_block("Attack1/aftermath.png","Rout",
+	[
+		"The troll auxiliaries seeing their masters being slaughtered wisely decided to run away."
+	]
+);
+the_walkthrough.create_block("Attack1/aftermath2.png","Aftermath",
+	[
+		"Unfortunately when Edzul had realized his mighty fighting skills something had to be forgotten. Mercy was forgotten.",
+		"The twisted, mangled troll corpses soon dotted the landscape."
+	]
+);
+the_walkthrough.create_block("Attack1/the_list.png","The score",
+	[
+		"When the nobles had totaled the dead, it read victory, but at a high cost."
+	]
+);
+the_walkthrough.create_block("Attack1/cleanup.png","Goblinite",
+	[
+		"Now the only thing left to do was the unenviable task of hauling the corpses of the dead and their belongings."
+	]
+);
+the_walkthrough.create_high_nav("top_menu");
--- DwarfFortressPlaythroughs/Fortress/Tinpractices/top_nav_seed.js
+++ DwarfFortressPlaythroughs/Fortress/Tinpractices/top_nav_seed.js
@@ -1,3 +1,3 @@
 function gen_stages(){
-	return ["Start","Were1","Industry1","Were2","Were3","Industry2"];
+	return ["Start","Were1","Industry1","Were2","Were3","Industry2","Attack1"];
 }

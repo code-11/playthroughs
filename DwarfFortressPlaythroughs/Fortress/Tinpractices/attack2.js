9598e1cce9d6f920d9be3cb4aa96295a90b58d64
code-11 <brendan.ritter@yahoo.com>
2016-02-29 14:18:37 -0500

    Got through another folder.

---
+++ DwarfFortressPlaythroughs/Fortress/Tinpractices/attack2.js
@@ -0,0 +1,90 @@
+var the_walkthrough=new walkthrough();
+
+the_walkthrough.create_block("Attack2/1_migrants.png","Migrants",
+	[
+		"Helpfully, just as soon as the attack was defeated a new wave of volunteers arrived at the fortress to replace the fallen."
+	]
+);
+the_walkthrough.create_block("Attack2/3_storage_1.png","Storage",
+	[
+		"The goblinite required more room than expected and additional storage had to be made.",
+		"Meanwhile the storage organization of the fortress had devolved into massive all-type piles sitting on different levels."
+	]
+);
+the_walkthrough.create_block("Attack2/4_grate.png","A grate idea",
+	[
+		"The nobles were worried about so many dwarves being trapped outside during attacks.",
+		"Therefore they build an indoor fishing area that connected to the canal.",
+		"The fisher dwarves refused to use it."
+	]
+);
+the_walkthrough.create_block("Attack2/5_overview_1.png","Overview",
+	[
+		"The dead shells of the river mussels were overloading the craftdwarves attempts to make things out of them",
+		"The massive surplus demanded new rooms to be built."
+	]
+);
+the_walkthrough.create_block("Attack2/6_storage_2.png","More storage",
+	[
+			"Determined to avoid a situation where the fortress ran out of storage space, the dwarves carved out a massive cavern and declared the whole thing as a storage center."
+	]
+);
+the_walkthrough.create_block("Attack2/7_attack_1.png","Attack!",
+	[
+			"Storage suddenly became the least of their problems. Another force of goblins, cheerfully provided by the evil Fly of Splashing had arrived."
+	]
+);
+the_walkthrough.create_block("Attack2/8_attack2_2.png","Trajectory",
+	[
+			"Although smaller, this force was much swifter in moving toward the fortress, perhaps because they didn't have to cross the river."
+	]
+);
+the_walkthrough.create_block("Attack2/9_attack_3.png","Battle plans",
+	[
+		"The military was vastly reduced in size even with the new arrivals. However, they bravely went out to meet the invaders.",
+		"Meanwhile, the haughty fisherdwarves who had spurned the safe indoor fishing area were again stuck outside as the entrance closed."	
+	]
+);
+the_walkthrough.create_block("Attack2/10_lord.png","Think of the puppies!",
+	[
+		"Edzul stopped when she heard the call to arms. Flashbacks to puppy corpses danced across her suddenly reddening vision. A hammer found its way into her hand somehow. It was time.",
+		"Such was the force of her onslaught that her enemies literally exploded."
+	]
+);
+the_walkthrough.create_block("Attack2/11_list.png","The list",
+	[
+		"This attack was much smaller, and had the added benefit of falling into the hunterdwarves carefully placed traps. Soon two of the enemy were taken captive.",
+		"A the moon appeared overhead, the cries of battle met the screams from below as the cursed dwarves transformed, sealed away in their walls."
+	]
+);
+the_walkthrough.create_block("Attack2/12_lord_2.png","Two hammers",
+	[
+		"The hammer lord Edzul was not the only one with blood on her hands. Another squad leader rose to the challenge."
+	]
+);
+the_walkthrough.create_block("Attack2/13_aftermath.png","Aftermath",
+	[
+		"The fallout was not as bad this time. One or two dwarves were injured but by in large the worst part of the attack was hauling all the clothing off the field."
+	]
+);
+the_walkthrough.create_block("Attack2/14_storage_3.png","A cave of corpses",
+	[
+		"Tired with looking out every day from their walls and seeing mounds of corpses, the nobles decided to move all corpses inside."
+	]
+);
+the_walkthrough.create_block("Attack2/15_storage_4.png","A caravan of bodies",
+	[
+		"Thus the great corpse migration started. It took the combined effort of most of the fortress."
+	]
+);
+the_walkthrough.create_block("Attack2/17_artifact_2.png","Artifact",
+	[
+		"Another horror, another artifact."
+	]
+);
+the_walkthrough.create_block("Attack2/18_oops.png","Oops",
+	[
+		"As the toxic miasma seeped through the doors to the corpse storage pile, the nobles were suddenly reminded why the didn't do this earlier."
+	]
+);
+the_walkthrough.create_high_nav("top_menu");

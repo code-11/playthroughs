e4be3fb7f83be8cec80aa7379358fe9b7f0bd89d
code-11 <brendan.ritter@yahoo.com>
2016-02-29 15:28:46 -0500

    Another folder down

---
+++ DwarfFortressPlaythroughs/Fortress/Tinpractices/industry3.js
@@ -0,0 +1,54 @@
+var the_walkthrough=new walkthrough();
+
+the_walkthrough.create_block("Industry3/1_list.png","List",
+	[
+		"The population of the fort had recovered. The farmers mostly kept everyone alive while the rest partied."
+	]
+);
+the_walkthrough.create_block("Industry3/2_overview.png","Defenses",
+	[
+		"Exterior walls were planned and readied."
+	]
+);
+the_walkthrough.create_block("Industry3/3_hammer.png","If I had a hammer...",
+	[
+		"This is my hammer, Edzul the hammer lord explained. There are many like it, but this one is mine."
+	]
+);
+the_walkthrough.create_block("Industry3/4_hammer.png","If I had a hammer...",
+	[
+		"The hammer was called the Ultimate Ford to commemorate the river crossing of the goblins from the first attack.",
+		"Quietly, Edzul added that the hammer was the ultimate ford across the river of life. This was too poetic for most of the assembled to grasp, so they ignored her."
+	]
+);
+the_walkthrough.create_block("Industry3/5_cavern.png","Oooh whats this?",
+	[
+		"After several months of furious digging, the miners finally came across the first cavern layer."
+	]
+);
+the_walkthrough.create_block("Industry3/6_artifact.png","Artifact",
+	[
+		"The first non-violent artifact was created. Dumed the artist tried to explain his rationale of making a silver chain.",
+		"It's symbolic!",
+		"A silver chain is useless! It will break!",
+		"It's not about the physical manifestation, it's about what the chain embodies...",
+		"The dwarves had enough and dispersed, leaving the frustrated artist to his own devices."
+	]
+);
+the_walkthrough.create_block("Industry3/7_overview.png","Praise the gods",
+	[
+		"Wait. We forgot to build a temple.",
+		"The dwarves looked at the noble inquisitively. Temples were rare and ancient things.",
+		"No, we can build one here!",
+		"The dwarves shrugged and made a temple. They forgot what god it was to. It was very popular though."
+	]
+);
+the_walkthrough.create_block("Industry3/8_mystery.png","One craftdwarf sitting in a tree...",
+	[
+		"As a noble walked past the fortifications he happened to spy a dwarf in a tree.",
+		"Dwarves don't belong in trees. What is that dwarf doing?",
+		"He had the tree cut down. Perhaps this is the key to the mystery of the dead dwarves in the tree?",
+		"The dwarf, now freed from the tree refused to talk about the experience."
+	]
+);
+the_walkthrough.create_high_nav("top_menu");

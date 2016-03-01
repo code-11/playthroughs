1d521ca1778a6f81419e6f4af1b915113d4bc0ee
code-11 <brendan.ritter@yahoo.com>
2016-03-01 00:21:15 -0500

    Added a next button that knows where to go. Also added a down arrow on the landing page of each chapter to indacate scrolling down.

--- Common/main.js
+++ Common/main.js
@@ -46,14 +46,20 @@ var walkthrough=function(){
 		for (var i=0; i<stages.length; i+=1){
 			var stage_link = document.createElement("a");
 			stage_link.className="stage_link";
-			stage_link.text=stages[i].charAt(0).toUpperCase() + stages[i].slice(1);
+			stage_link.text="Ch"+i//.charAt(0).toUpperCase() + stages[i].slice(1);
 			stage_link.href=stages[i]+".html"
 			stagebar.appendChild(stage_link);
 			if (stages[i]+".html"==cur_loc){
 				stage_link.className+=" selected-link";
 				this.create_navbar(stage_nav_id);
+				if (i!=stages.length-1){
+					this.create_next(i);
+				}else{
+					this.no_next();
+				}
 			}
 		}
+		
 	}
 	this.create_navbar=function(navbar_id){
 		var navbar=document.getElementById(navbar_id);
@@ -65,6 +71,20 @@ var walkthrough=function(){
 			navbar.appendChild(anchor_start);
 		}
 	}
-
+	this.create_next=function(cur_num)
+	{
+		var stages=gen_stages();
+		var cur_loc=location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+		var next_num=cur_num+1;
+		var next_stage=stages[next_num]
+		var the_root=location.pathname.substring(0,location.pathname.lastIndexOf("/") + 1);
+		var next_loc=the_root+next_stage+".html";
+		var next_btn=document.getElementById("next-btn");
+		next_btn.href=next_loc;
+	}
+	this.no_next=function(){
+		var next_btn=document.getElementById("next-btn");
+		next_btn.style.display="none";
+	}
 }
 
